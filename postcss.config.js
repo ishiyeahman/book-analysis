d0bf631c5794d1e71948574e985f81b48d07fe07
ishiyeahman <ishiyeahman@gmail.com>
2022-10-11 09:15:12 +0900

    fix webpack ...

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./app/javascript/packs/tailwind.config.js'), // 追加
-    require('autoprefixer'),                                             // 追加
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
